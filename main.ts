4ffcaae8aeeade92173d5a6b79f3b51419f5517b
Anshulkammi <122560115+Anshulkammi@users.noreply.github.com>
2023-01-13 13:15:21 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,4 +4,4 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
 })
-basic.showIcon(IconNames.Giraffe)
+basic.showIcon(IconNames.House)
