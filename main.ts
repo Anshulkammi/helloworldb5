82a03fcb5ddb9073b25a1ef3c7aecb6b1201e39c
Anshulkammi <122560115+Anshulkammi@users.noreply.github.com>
2023-01-13 13:29:28 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Chessboard)
+})
 basic.showIcon(IconNames.House)
 basic.forever(function () {
     music.playMelody("C5 A B G A F G E ", 500)
