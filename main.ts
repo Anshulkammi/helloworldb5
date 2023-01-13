d618da565815bac7f9852378caec9981093697dc
Anshulkammi <122560115+Anshulkammi@users.noreply.github.com>
2023-01-13 13:27:09 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,6 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
 })
 basic.showIcon(IconNames.House)
+basic.forever(function () {
+    music.playMelody("C5 A B G A F G E ", 500)
+})
